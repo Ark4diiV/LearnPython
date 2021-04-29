ab6db986fbfa7cd928e8da6a1e9d93a0d5085de8
Ark4diiV <arkady.vashchuk@outlook.com>
2021-04-30 01:02:09 +0300

    array_count9 v.1.0

---
+++ Warmup-2/array_count9.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Given an array of ints, return the number of 9's in the array.
+
+def array_count9(nums):
+  count = 0
+  for i in nums:
+    if i == 9:
+      count += 1
+      
+  return count
+
+print(array_count9([1, 2, 9]))
+print(array_count9([1, 9, 9]))
+print(array_count9([1, 9, 9, 3, 9]))
