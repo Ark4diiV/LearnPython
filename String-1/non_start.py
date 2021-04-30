4195e9f9e3886e1ee4613779df9698cddabfbfeb
Ark4diiV <arkady.vashchuk@outlook.com>
2021-05-01 01:32:22 +0300

    non_start v.1.0

---
+++ String-1/non_start.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Given 2 strings, return their concatenation, except omit the first char of each. 
+# The strings will be at least length 1.
+
+def non_start(a, b):
+  return a[1:] + b[1:]
+
+print(non_start('shotl', 'java'))
+print(non_start('mart', 'dart'))
+print(non_start('Hello', 'There'))
