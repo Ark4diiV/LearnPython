2ca89d29be51f4c7d1f409d3083329119be9fc90
Ark4diiV <arkady.vashchuk@outlook.com>
2021-05-01 00:29:00 +0300

    hello_name v.1.0

---
+++ String-1/hello_name.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+def hello_name(name):
+  return("Hello " + name + "!")
+
+print(hello_name('Alice'))
+print(hello_name('Omega'))
+print(hello_name('ho ho ho'))
